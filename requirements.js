971575845b4aef7ece4ccd2e2524cc1c19eaf09d
Domaalva <139288690+Domaalva@users.noreply.github.com>
2026-06-18 06:22:13 +0000

    Agregar requirements

---
+++ requirements.js
@@ -0,0 +1,13 @@
+const getSalesCoffee = () => {
+
+    const request = new XMLHttpRequest();
+
+    request.open(
+        "GET",
+        "https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Coffee/Coffe_sales.xml"
+    );
+
+    request.send();
+
+    return request;
+};
