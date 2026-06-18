83432b22326d5f563c2d721f2e756dab0b1cff96
Domaalva <139288690+Domaalva@users.noreply.github.com>
2026-06-18 06:34:09 +0000

    Correccion index.js

--- index.js
+++ index.js
@@ -6,37 +6,40 @@ const processSalesCoffee = () => {
 
         const xml = request.responseXML;
 
-        const sales = xml.getElementsByTagName("sale");
+        const sales = xml.getElementsByTagName("row");
 
         const tbody = document.querySelector("#salesTable tbody");
 
         for (let sale of sales) {
 
             const date =
-                sale.getElementsByTagName("date")[0].textContent;
+                sale.getElementsByTagName("Date")[0].textContent;
 
             const product =
-                sale.getElementsByTagName("product")[0].textContent;
+                sale.getElementsByTagName("coffee_name")[0].textContent;
 
-            const quantity =
-                sale.getElementsByTagName("quantity")[0].textContent;
+            const money =
+                sale.getElementsByTagName("money")[0].textContent;
 
-            const total =
-                sale.getElementsByTagName("total")[0].textContent;
+            const payment =
+                sale.getElementsByTagName("cash_type")[0].textContent;
 
             tbody.innerHTML += `
-                <tr>
-                    <td>${date}</td>
-                    <td>${product}</td>
-                    <td>${quantity}</td>
-                    <td>${total}</td>
+                <tr class="border-b border-amber-300">
+                    <td class="py-2 px-4">${date}</td>
+                    <td class="py-2 px-4">${product}</td>
+                    <td class="py-2 px-4">$${money}</td>
+                    <td class="py-2 px-4">${payment}</td>
                 </tr>
             `;
         }
+    };
 
-        new DataTable("#salesTable");
+    request.onerror = () => {
+        alert("Error al cargar los datos.");
     };
 };
+
 window.onload = () => {
     processSalesCoffee();
 };
